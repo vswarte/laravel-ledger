efe10c705bc6000fca7546154559b6c015bea3d4
Vincent Swarte <vincent@moodles.nl>
2018-02-26 14:21:10 +0100

    added accessor and mutator to mutation class

--- src/LedgerMutation.php
+++ src/LedgerMutation.php
@@ -2,6 +2,7 @@
 
 namespace Ledger;
 
+use Money\Money;
 use Illuminate\Database\Eloquent\Model;
 
 class LedgerMutation extends Model
@@ -16,5 +17,18 @@ class LedgerMutation extends Model
     public function account()
     {
         return $this->belongsTo(LedgerAccount::class);
+    }
+
+    public function getAmountAttribute($value)
+    {
+        return Money::EUR($value);
+    }
+
+    public function setAmountAttribute($value)
+    {
+        if ($value instanceof Money)
+            $value = $value->getAmount();
+
+        $this->attributes['amount'] = $value;
     }
 }
