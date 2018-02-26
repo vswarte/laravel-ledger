3f3119d59a6533e84b4646d6aa8fcf24b3e57bcd
Vincent Swarte <vincent@moodles.nl>
2018-02-26 14:14:50 +0100

    added mutations relation to transactions

--- src/LedgerTransaction.php
+++ src/LedgerTransaction.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class LedgerTransaction extends Model
 {
     protected $fillable = ['description'];
+
+    public function mutations()
+    {
+        return $this->hasMany(LedgerTransaction::class);
+    }
 }
